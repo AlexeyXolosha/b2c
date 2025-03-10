e1708b42da285a47db58e62d20d538821370b83a
AlexeyXolosha <alexeyxolosh4@gmail.com>
2025-03-10 16:01:14 +0500

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt']
+  modules: ['@pinia/nuxt'],
+  css: ['~/assets/style/main.css'],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/_colors.scss" as *;'
+        }
+      }
+    }
+  }
 })
