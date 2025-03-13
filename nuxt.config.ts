67d5ac954de02e2668dfbec4d4f2e0423c76efa9
AlexeyXolosha <alexeyxolosh4@gmail.com>
2025-03-13 11:53:37 +0500

    updated 13.03

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,15 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
-  css: ['~/assets/style/main.css'],
+  css: ['~/assets/style/main.css', '~/assets/fonts/font-awesome/css/all.css'],
 
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "~/assets/_colors.scss" as *;'
+          additionalData: '@use "~/assets/style/_vars.scss" as *;'
         }
       }
     }
   }
 })
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <NuxtLayout name="main">
     <div>
-    Привет
     </div>
   </NuxtLayout>
 
