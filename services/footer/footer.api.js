497a28922c4a34186cbb1833eb0800e3c9a38d2d
AlexeyXolosha <alexeyxolosh4@gmail.com>
2025-05-13 17:53:32 +0500

    footer rest full

--- services/footer/footer.api.js
+++ services/footer/footer.api.js
@@ -2,6 +2,22 @@ import {useFetchHook} from "~/composables/useFetchHook.js";
 
 export default {
     async GET_STORES() {
-            return useFetchHook('/api/proxy/include/menu/footer/', {method: "GET"}, "FOOTER_STORES", { server: true })
+        return useFetchHook('/api/proxy/include/menu/stores/', {method: "GET"}, "FOOTER_STORES", {server: true})
+    },
+
+    async GET_FOOTER_MENU() {
+        return useFetchHook('/api/proxy/include/menu/footer/', {method: "GET"}, "FOOTER_MENU", {server: true})
+    },
+
+    async GET_FOOTER_INFO() {
+        return useFetchHook('/api/proxy/include/menu/info/', {method: "GET"}, "FOOTER_INFO", {server: true})
+    },
+
+    async GET_FOOTER_TELEPHONE() {
+        return useFetchHook('/include/static-info/phone/', {method: "GET"}, "FOOTER_TELEPHONE", {server: true})
+    },
+
+    async GET_FOOTER_SOCIAL() {
+        return useFetchHook('/include/static-info/social-media/', {method: "GET"}, "FOOTER_SOCIAL", {server: true})
     }
 }
